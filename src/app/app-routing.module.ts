aca4538cefdd2df628202edfa3ff1daac6ac1cd1
Davide Carita <davide.carita@alten.it>
2024-05-28 11:40:59 +0200

    song detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CallbackComponent } from './auth/callback.component';
+import { AppComponent } from './app.component';
+import { ReeSongDetailComponent } from './components/ree-song-detail/ree-song-detail.component';
+import { ReeHomeComponent } from './components/ree-home/ree-home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', component: ReeHomeComponent},
+  { path: 'track/:trackId', component: ReeSongDetailComponent},
   { path: 'callback', component: CallbackComponent }
 ];
 
--- src/app/components/ree-song-detail/ree-song-detail.component.ts
+++ src/app/components/ree-song-detail/ree-song-detail.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Song } from '../../model/Song';
+import { ActivatedRoute } from '@angular/router';
+import { ReecordsService } from '../../services/reecords.service';
+import { Artist } from '../../model/Artist';
 
 @Component({
-  selector: 'app-ree-song-detail',
+  selector: 'ree-song-detail',
   templateUrl: './ree-song-detail.component.html',
   styleUrl: './ree-song-detail.component.scss'
 })
-export class ReeSongDetailComponent {
+export class ReeSongDetailComponent implements OnInit{
+  song: Song = new Song('IDIDIDID','NAMENAME', [
+    new Artist('', 'ARTISTARTIST', []),
+    new Artist('', 'ARTIST22222', [])
+  ], 'ALBUMALBUM');
+
+  constructor(private _route: ActivatedRoute, private _reecordsService: ReecordsService) {}
+
+  ngOnInit(): void {
+    if (history.state.track) {
+      this.song = history.state.track;
+      return;
+    }
+
+    this._route.url.subscribe(segments => {
+      let trackID = segments[segments.length-1].toString();
+      this.findTrackById(trackID);
+    })
+  }
+
+  private findTrackById(trackID: string) {
+    this._reecordsService.getSongById(trackID).subscribe(
+      track => {
+        this.song = track;
+      }
+    );
+  }
 
 }
